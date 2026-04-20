c87707f8a4b66b7ed74bc94da45398b263263116
Dravid <supermegaawesomedude3460@yahoo.com>
2026-04-19 17:47:02 -0700

    finished switch tab panel part of function

--- scripts/modeTabs.js
+++ scripts/modeTabs.js
@@ -4,4 +4,19 @@
     GlobalModeTabButtons[GlobalCurrentMode.get()].setAttribute("aria-selected",false);
     GlobalModeTabButtons[newMode].classList.add("modetab-selected");
     GlobalModeTabButtons[newMode].setAttribute("aria-selected",true);
+    //Switch tab panel
+    GlobalModeTabPanels[GlobalCurrentMode.get()].classList.add("hidden");
+    GlobalModeTabPanels[newMode].classList.remove("hidden");
+    GlobalCurrentMode.set(newMode); //Change mode
+    GlobalFocusedMode.set(newMode); //Change focused mode
+    if (GlobalHistoryLogging) {
+        const historyObj = {
+            page: GlobalModeNumbersToModes.get(newMode),
+            mode: newMode,
+            path: GlobalModeNumbersToRoutes.get(newMode)  
+        };   
+        history.pushState(historyObj, "", historyObj.path);
+        console.log("Console: In switchMode; pushing state: ", 
+            JSON.stringify(historyObj));
+    }
 }
